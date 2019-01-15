354dbab811ee0f625579af406ed734399a868eb5
brinkhoff98 <44202147+brinkhoff98@users.noreply.github.com>
2019-01-15 12:39:49 +0100

    meme

---
+++ WindowsFormsApp4/WindowsFormsApp4/list.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    class list
+    {
+        public void Add()
+        {
+
+        }
+        public void Count()
+        {
+
+        }
+        public void RemoveAt()
+        {
+
+        }
+    }
+}
