afd9b8ca9dd3837cd68c23c2bc5639686a652725
Haleballe <oskarmorell1998@hotmail.se>
2019-01-18 11:11:51 +0100

    f

--- WindowsFormsApp4/WindowsFormsApp4/Game.cs
+++ WindowsFormsApp4/WindowsFormsApp4/Game.cs
@@ -11,5 +11,30 @@ namespace WindowsFormsApp4
         int Dealerscore = 0;
         int Playerscore = 0;
 
+        public Card Drawcard()
+        {
+            Mycard = Deck.GetCard();
+            return Mycard;
+        }
+
+        public bool Checkscore()
+        {
+            if (Playerscore > Dealerscore)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Addscoreplayer(Mycard)
+        {
+            Playerscore += Mycard.number;
+        }
+        public void Addscoredealer(Mycard)
+        {
+            Dealerscore += Mycard.number;
+        }
     }
 }
---
+++ WindowsFormsApp4/WindowsFormsApp4/Myrandom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    public class Myrandom
+    {
+        public int Newrandom(int cardcount)
+        {
+            Random random = new Random();
+            
+            return random.Next(0, (cardcount + 1));
+        }
+    }
+}
